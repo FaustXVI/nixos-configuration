d5b117c22deb3cd644ef79d262b33c36b6431164
Xavier Detant <xavier.detant@gmail.com>
2016-11-23 14:27:39 +0100

    Prepare for user update

--- users.nix
+++ users.nix
@@ -2,6 +2,7 @@
 
 {
 	users = {
+		mutableUsers = true; # when updating, set this to true, rebuild, then go back to false and rebuild and re change the password via passwd
 		extraUsers = {
 			xadet = {
 				isNormalUser = true;
