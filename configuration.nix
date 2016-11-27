1453da8c83c3c358fea17a51027a3b64234339c6
Xavier Detant <xavier.detant@gmail.com>
2016-11-27 14:44:44 +0100

    Configuration pour le fixe

--- configuration.nix
+++ configuration.nix
@@ -18,9 +18,11 @@
 			./virtualisation.nix
 		];
 
-	boot.loader.systemd-boot.enable = true;
-	boot.loader.efi.canTouchEfiVariables = true;
 
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+
+  boot.loader.grub.device = "/dev/sdb"; # or "nodev" for efi only
 	i18n = {
 		consoleFont = "Lat2-Terminus16";
 		consoleKeyMap = "fr";
