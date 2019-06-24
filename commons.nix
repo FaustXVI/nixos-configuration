f30da43931799acec0120d8660bb424cd464e49e
FaustXVI <xavier.detant@gmail.com>
2019-06-24 22:57:07 +0200

    Add yubikey for ssh

--- commons.nix
+++ commons.nix
@@ -49,12 +49,25 @@
 			package = pkgs.pulseaudioFull;
 		};
 	};
+	services = {
+		pcscd = {
+			enable = true;
+		};
+	};
 	programs = {
+		ssh.startAgent = false;
 		gnupg = {
 			agent = {
 				enable = true;
 				enableSSHSupport = true;
 			};
 		};
+	};
+	environment = {
+		shellInit = ''
+  export GPG_TTY="$(tty)"
+  gpg-connect-agent /bye
+  export SSH_AUTH_SOCK="/run/user/$UID/gnupg/S.gpg-agent.ssh"
+		'';
 	};
 }
