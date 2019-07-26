ba8dab98eaa309f46f040f988f65631bac86e2a2
FaustXVI <xavier.detant@gmail.com>
2019-07-26 18:13:34 +0200

    use label for decryption in luks file

---
+++ luks.nix
@@ -0,0 +1,24 @@
+{ config, lib, pkgs, ... }:
+
+{
+  boot.initrd.kernelModules = ["vfat" "nls_cp437" "nls_iso8859-1" "usbhid" "uas" "usbcore" "usb_storage"];
+  boot.initrd.luks = {
+    # Update if necessary
+    cryptoModules = ["aes" "xts" "sha512"];
+
+    # Support for Yubikey PBA
+    yubikeySupport = true;
+
+    devices."key-cyphered".yubikey = {
+        slot = 2;
+        twoFactor = true; # Set to false for 1FA
+        gracePeriod = 30; # Time in seconds to wait for Yubikey to be inserted
+        keyLength = 64; # Set to $KEY_LENGTH/8
+        saltLength = 16; # Set to $SALT_LENGTH
+
+        storage = {
+          device = "/dev/disk/by-label/SYSTEM"; # Be sure to update this to the correct volume
+        };
+      };
+  };
+}
