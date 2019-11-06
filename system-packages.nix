33ce0a0dbafd5fcf141cb589f5741c19a878d41c
FaustXVI <xavier.detant@gmail.com>
2019-11-06 10:48:20 +0100

    fonts

--- system-packages.nix
+++ system-packages.nix
@@ -1,47 +1,69 @@
 { config, pkgs, ... }:
 
 {
-	environment = {
-		variables = {
-			EDITOR = pkgs.lib.mkOverride 0 "vim";
-		};
-		systemPackages = with pkgs; [
-				wget
-				git
-				vim
-				usbutils
-				tree
-				gnupg
-				file
-				gnumake
-				automake
-				gcc
-				aspell
-				aspellDicts.en
-				aspellDicts.fr
-				direnv
-				terminator
-				zip
-				unzip
-				udiskie
-				apulse
-				pavucontrol
-				pasystray
-				autorandr
-				powerline-fonts
-				xorg.xbacklight
-				yubico-piv-tool
-				yubikey-personalization
-				yubikey-personalization-gui
-				yubioath-desktop
-		];
-	};
-	programs = {
-		bash = {
-			enableCompletion = true;
-		};
-		ssh = {
-			startAgent = false;
-		};
-	};
+  environment = {
+    variables = {
+      EDITOR = pkgs.lib.mkOverride 0 "vim";
+    };
+    systemPackages = with pkgs; [
+      wget
+      git
+      vim
+      usbutils
+      tree
+      gnupg
+      file
+      gnumake
+      automake
+      gcc
+      aspell
+      aspellDicts.en
+      aspellDicts.fr
+      direnv
+      terminator
+      zip
+      unzip
+      udiskie
+      apulse
+      pavucontrol
+      pasystray
+      autorandr
+      powerline-fonts
+      xorg.xbacklight
+      yubico-piv-tool
+      yubikey-personalization
+      yubikey-personalization-gui
+      yubioath-desktop
+    ];
+  };
+  fonts = {
+    enableDefaultFonts = true;
+    fonts = with pkgs; [
+      noto-fonts
+      noto-fonts-cjk
+      noto-fonts-emoji
+      liberation_ttf
+      fira-code
+      fira-code-symbols
+      mplus-outline-fonts
+      dina-font
+      proggyfonts
+    ];
+    fontconfig = {
+      penultimate.enable = false;
+      defaultFonts = {
+        monospace = [ "Fira Code" ];
+        sansSerif = [ "Noto Sans" ];
+        serif     = [ "Noto Serif" ];
+      };
+    };
+  };
+  programs = {
+    bash = {
+      enableCompletion = true;
+    };
+    ssh = {
+      startAgent = false;
+    };
+  };
 }
