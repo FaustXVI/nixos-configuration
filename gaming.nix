4397f6f4e2670b064905c07e46161a741cc924b6
FaustXVI <xavier.detant@gmail.com>
2020-04-25 16:15:11 +0200

    Make xbox controller work

--- gaming.nix
+++ gaming.nix
@@ -1,16 +1,18 @@
 { config, pkgs, ... }:
 
 {
-	environment = {
-		systemPackages = with pkgs; [
-			steam
-			discord
-		];
-	};
+  environment = {
+    systemPackages = with pkgs; [
+      steam
+      discord
+      xboxdrv
+    ];
+  };
 
-	hardware = {
-		opengl = {
-			driSupport32Bit = true;
-		};
-	};
+  hardware = {
+    opengl = {
+      driSupport32Bit = true;
+      extraPackages32 = [ pkgs.pkgsi686Linux.libva ];
+    };
+  };
 }
