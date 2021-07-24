8f7534dd72e6d5c0f84c943cff358b26884e97c0
FaustXVI <xavier.detant@gmail.com>
2021-07-24 14:05:38 +0200

    Revert debug

--- system-packages.nix
+++ system-packages.nix
@@ -28,7 +28,7 @@
       pavucontrol
       pasystray
       autorandr
-  #    powerline-fonts
+      powerline-fonts
       xorg.xbacklight
       yubico-piv-tool
       yubikey-personalization
@@ -38,27 +38,27 @@
       htop
     ];
   };
-  #fonts = {
-  #  enableDefaultFonts = true;
-  #  fonts = with pkgs; [
-  #    noto-fonts
-  #    noto-fonts-cjk
-  #    noto-fonts-emoji
-  #    liberation_ttf
-  #    fira-code
-  #    fira-code-symbols
-  #    mplus-outline-fonts
-  #    dina-font
-  #    proggyfonts
-  #  ];
-  #  fontconfig = {
-  #    defaultFonts = {
-  #      monospace = [ "Fira Code" ];
-  #      sansSerif = [ "Noto Sans" ];
-  #      serif     = [ "Noto Serif" ];
-  #    };
-  #  };
-  #};
+  fonts = {
+    enableDefaultFonts = true;
+    fonts = with pkgs; [
+      noto-fonts
+      noto-fonts-cjk
+      noto-fonts-emoji
+      liberation_ttf
+      fira-code
+      fira-code-symbols
+      mplus-outline-fonts
+      dina-font
+      proggyfonts
+    ];
+    fontconfig = {
+      defaultFonts = {
+        monospace = [ "Fira Code" ];
+        sansSerif = [ "Noto Sans" ];
+        serif     = [ "Noto Serif" ];
+      };
+    };
+  };
   programs = {
     bash = {
       enableCompletion = true;
