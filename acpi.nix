2fb83ce6ee2c2cc67c0292b3403a007030428ef8
FaustXVI <xavier.detant@gmail.com>
2022-03-18 09:47:07 +0100

    Do not shut down on button pressed

--- acpi.nix
+++ acpi.nix
@@ -12,4 +12,12 @@
 		};
 	};
 
+    services = {
+      logind = {
+        extraConfig = ''
+        # don’t shutdown when power button is short-pressed
+        HandlePowerKey=ignore
+        '';
+      };
+    };
 }
