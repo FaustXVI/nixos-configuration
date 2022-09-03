4537ba7e77708f936188390306b2f355cd6d8bce
FaustXVI <xavier.detant@gmail.com>
2022-09-03 14:06:34 +0200

    Add rules to flash keyboard

--- keyboard.nix
+++ keyboard.nix
@@ -2,6 +2,14 @@
 
 {
 	services = {
+        udev = {
+            extraRules = ''
+ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789B]?", ENV{ID_MM_DEVICE_IGNORE}="1"
+ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789A]?", ENV{MTP_NO_PROBE}="1"
+SUBSYSTEMS=="usb", ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789ABCD]?", MODE:="0666"
+KERNEL=="ttyACM*", ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789B]?", MODE:="0666"
+      '';
+        };
 		xserver =  {
             autoRepeatDelay = 200;
 			layout = "fr";
