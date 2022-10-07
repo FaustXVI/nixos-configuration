c73e99ab90e3dd55d12325804e7c01a5696807c8
FaustXVI <xavier.detant@gmail.com>
2022-10-07 10:18:14 +0200

    Fix nix-rebuild issue

--- network.nix
+++ network.nix
@@ -12,6 +12,9 @@
 
   networking.extraHosts = "127.0.0.1 nixos";
 
+  # See: https://github.com/NixOS/nixpkgs/issues/180175
+  systemd.services.NetworkManager-wait-online.enable = false;
+
   services={
     openssh = {
       enable = true;
