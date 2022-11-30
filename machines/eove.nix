80709257783557ec1e0c1efb793f5cdc7bff1d2b
FaustXVI <xavier.detant@gmail.com>
2022-11-30 11:35:27 +0100

    Eove machine
    Kernel 6 seems necessary for HDMI

--- machines/eove.nix
+++ machines/eove.nix
@@ -11,7 +11,10 @@
     ../luks.nix
   ];
 
+  hardware.video.hidpi.enable = true;
   boot = {
+    kernelPackages = pkgs.linuxPackages_latest;
+    initrd.kernelModules = [ "dm-snapshot" ];
     loader = {
       systemd-boot = {
         enable = true;
@@ -20,11 +23,22 @@
         canTouchEfiVariables = true; 
       };
     };
+    kernelParams = [ "i915" "acpi_osi=linux" "module_blacklist=hid_sensor_hub" ];
   };
   services = {
+    xserver = {
+      dpi = 96;
+#      videoDrivers = [ "intel"  ];
+    };
     printing = {
       enable = true;
       drivers = [ pkgs.gutenprint ];
     };
   };
+  hardware = {
+    bluetooth = {
+      enable = true;
+    };
+  };
+  programs.light.enable = true;
 }
