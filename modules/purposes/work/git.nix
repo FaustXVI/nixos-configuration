bffd2f6d54a99fc177c9b0174aeab14668d78b78
Xavier Detant <1016863+FaustXVI@users.noreply.github.com>
2023-02-03 14:30:11 +0100

    Add git subrepo for work

--- modules/purposes/work/git.nix
+++ modules/purposes/work/git.nix
@@ -2,9 +2,15 @@
 
 {
   config = mylib.mkIfComputerHasPurpose "work" {
+    environment = {
+      systemPackages = with pkgs; [
+        git-subrepo
+      ];
+    };
+
     home-manager.users.xadet = {
       programs.git = {
-            userName = "Xavier Detant";
+        userName = "Xavier Detant";
       };
     };
   };
