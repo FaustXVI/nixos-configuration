cbab8d7f31b4598016a49ee2c71b8f57df912ec7
FaustXVI <1016863+FaustXVI@users.noreply.github.com>
2023-02-03 21:23:31 +0100

    Configure hardware of desktop

--- machines/desktop-home.nix
+++ machines/desktop-home.nix
@@ -1,20 +1,29 @@
 { config, pkgs, ... }:
 
-{
+let
+  hard = builtins.fetchGit { 
+    url = "https://github.com/NixOS/nixos-hardware.git";
+    rev = "b7ac0a56029e4f9e6743b9993037a5aaafd57103";
+  };
+in {
   imports =
     [
+      "${hard}/common/cpu/amd"
+      "${hard}/common/cpu/amd/pstate.nix"
+      "${hard}/common/gpu/nvidia"
+      "${hard}/common/pc"
+      "${hard}/common/pc/ssd"
       ../hardware-configuration.nix
       ../modules
     ];
-  xadetComputer = {
-    type = "desktop";
-    purposes = [ "home" "gaming" "youtube" "photo" ];
-  };
+    xadetComputer = {
+      type = "desktop";
+      purposes = [ "home" "gaming" "youtube" "photo" ];
+    };
 
     services = {
       xserver = {
         dpi = 96;
-        videoDrivers = [ "nvidia" ];
         displayManager = {
           sessionCommands = ''
                     xrandr --output DP-3 --mode 1920x1080 --output HDMI-0 --mode 2560x1080 --left-of DP-3 --primary &
