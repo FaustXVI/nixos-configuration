90de4d68119fc5ddeb61321a3e937e921706f51e
Xavier Detant <1016863+FaustXVI@users.noreply.github.com>
2023-02-04 10:45:33 +0100

    Automatic home state version

--- modules/system/home-manager/xadet.nix
+++ modules/system/home-manager/xadet.nix
@@ -3,7 +3,7 @@
   imports = mylib.importAllFilteredWith (n: n != "root.nix" && n != "xadet.nix") args ./.;
   config = {
     home = {
-      stateVersion = "22.05";
+      stateVersion = config.system.stateVersion;
       enableNixpkgsReleaseCheck = true;
     };
     programs = {
