26b3e4ef988d57c3ecfe9bac1cbddec141054ae7
Xavier Detant <1016863+FaustXVI@users.noreply.github.com>
2023-02-06 09:43:21 +0100

    WIP terminator

--- modules/system/home-manager/shells/terminator.nix
+++ modules/system/home-manager/shells/terminator.nix
@@ -1,4 +1,40 @@
 {...}:
-{
+let
+  font = "Fira Code Light 16";
+  black = "#00000";
+  white = "#ffffff";
+  cursor = "#aaaaaa";
+in {
   xdg.configFile."terminator/config".source = ./terminator.config ;
+#  xdg.configFile."terminator/config".text = ''
+#[global_config]
+#  always_split_with_profile = True
+#  title_use_system_font = False
+#  title_font = ${font}
+#  putty_paste_style_source_clipboard = True
+#[keybindings]
+#[profiles]
+#  [[default]]
+#    foreground_color = "${white}"
+#    cursor_color = "${cursor}"
+#    font = ${font}
+#    scrollback_infinite = True
+#    use_system_font = False
+#  [[projector]]
+#    foreground_color = "${black}"
+#    background_color = "${white}"
+#    cursor_color = "${cursor}"
+#    font = ${font}
+#    scrollback_infinite = True
+#    use_system_font = False
+#[layouts]
+#  [[default]]
+#    [[[child1]]]
+#      parent = window0
+#      type = Terminal
+#    [[[window0]]]
+#      parent = ""
+#      type = Window
+#[plugins]
+#    '';
 }
