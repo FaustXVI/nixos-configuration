a0b11fcddbcb272772d4e88e43e4b4732dfe2eaa
FaustXVI <1016863+FaustXVI@users.noreply.github.com>
2023-02-08 20:58:03 +0100

    Configure channels dynamically

--- modules/system/home-manager/root.nix
+++ modules/system/home-manager/root.nix
@@ -1,5 +1,12 @@
-{ config, ... }: {
+{ config, ... }: 
+let
+  channelVersion = "22.11";
+in {
   xdg.configFile."nixpkgs/config.nix".source = ./nix/config.nix ;
-  home.file.".nix-channels".source = ./nix/nix-channels-root ;
+  home.file.".nix-channels".text = ''
+https://github.com/nix-community/home-manager/archive/release-${channelVersion}.tar.gz home-manager
+https://nixos.org/channels/nixos-${channelVersion} nixos
+https://nixos.org/channels/nixos-unstable nixos-unstable
+    '';
   home.stateVersion = config.system.stateVersion;
 }
