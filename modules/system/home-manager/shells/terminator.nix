5c93c6ec45f3b30cfda8bf0c9c3370a232673fb8
FaustXVI <1016863+FaustXVI@users.noreply.github.com>
2023-02-11 20:59:44 +0100

    Configure terminator

--- modules/system/home-manager/shells/terminator.nix
+++ modules/system/home-manager/shells/terminator.nix
@@ -1,40 +1,36 @@
 {...}:
 let
   font = "Fira Code Light 16";
-  black = "#00000";
-  white = "#ffffff";
-  cursor = "#aaaaaa";
+  black = "#111111";
+  white = "#eeeeee";
+  cursor = "#999999";
 in {
-  xdg.configFile."terminator/config".source = ./terminator.config ;
-#  xdg.configFile."terminator/config".text = ''
-#[global_config]
-#  always_split_with_profile = True
-#  title_use_system_font = False
-#  title_font = ${font}
-#  putty_paste_style_source_clipboard = True
-#[keybindings]
-#[profiles]
-#  [[default]]
-#    foreground_color = "${white}"
-#    cursor_color = "${cursor}"
-#    font = ${font}
-#    scrollback_infinite = True
-#    use_system_font = False
-#  [[projector]]
-#    foreground_color = "${black}"
-#    background_color = "${white}"
-#    cursor_color = "${cursor}"
-#    font = ${font}
-#    scrollback_infinite = True
-#    use_system_font = False
-#[layouts]
-#  [[default]]
-#    [[[child1]]]
-#      parent = window0
-#      type = Terminal
-#    [[[window0]]]
-#      parent = ""
-#      type = Window
-#[plugins]
-#    '';
+programs.terminator = {
+  enable = true;
+  config = {
+    global_config = {
+      always_split_with_profile = true;
+      title_use_system_font = false;
+      title_font = "${font}";
+      putty_paste_style_source_clipboard = true;
+    };
+    profiles = {
+      "default" = {
+        foreground_color = "${white}";
+        cursor_color = "${cursor}";
+        font = "${font}";
+        scrollback_infinite = true;
+        use_system_font = false;
+      };
+      "projector" = {
+        foreground_color = "${black}";
+        background_color = "${white}";
+        cursor_color = "${cursor}";
+        font = "${font}";
+        scrollback_infinite = true;
+        use_system_font = false;
+      };
+    };
+  };
+};
 }
