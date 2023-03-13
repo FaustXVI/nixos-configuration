d99ccba922204b0e5b2a7e46d94917acc156d505
Xavier Detant <1016863+FaustXVI@users.noreply.github.com>
2023-03-13 11:05:39 +0100

    Add github token to nix

--- modules/system/nix.nix
+++ modules/system/nix.nix
@@ -9,12 +9,17 @@
   };
 
   nixpkgs.config.allowUnfree = true;
+  sops.secrets.extra-nix-conf = {
+    format = "binary";
+    sopsFile = ./secrets/extra-nix.conf;
+  };
   nix = {
     settings = {
       experimental-features = [ "nix-command" "flakes" ];
     };
     extraOptions = ''
       min-free = ${toString (10 * 1024 * 1024 * 1024)}
+      !include ${config.sops.secrets.extra-nix-conf.path}
     '';
     gc = {
       dates = "13:10";
