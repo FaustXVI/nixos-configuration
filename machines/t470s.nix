e520a5db0eedcc2cab80472d9848f5d0f790764d
FaustXVI <1016863+FaustXVI@users.noreply.github.com>
2023-03-19 00:12:52 +0100

    Fix t470s hardware

--- machines/t470s.nix
+++ machines/t470s.nix
@@ -3,6 +3,10 @@
 {
   imports =
     [
+      "${builtins.fetchGit {
+        url = "https://github.com/NixOS/nixos-hardware.git";
+        rev = "b7ac0a56029e4f9e6743b9993037a5aaafd57103";
+      }}/lenovo/thinkpad/t470s"
       ../hardware-configuration.nix
       ../modules
     ];
