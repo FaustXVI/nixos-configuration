e0a8125ff5eb5cd333126303fc825c3d56198427
Xavier Detant <1016863+FaustXVI@users.noreply.github.com>
2023-03-28 18:20:54 +0200

    Fix gpg access to yubikey

--- modules/system/home-manager/crypto/gpg.nix
+++ modules/system/home-manager/crypto/gpg.nix
@@ -3,6 +3,9 @@ with pkgs; {
   programs = {
     gpg = {
       enable = true;
+      scdaemonSettings = {
+        disable-ccid = true;
+      };
       publicKeys = [
         { source = ./xadet-public.key; }
       ];
--- modules/system/shells.nix
+++ modules/system/shells.nix
@@ -5,10 +5,6 @@
     systemPackages = with pkgs; [
       terminator
     ];
-    shellInit = ''
-      gpg-connect-agent /bye
-      export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
-    '';
   };
   programs = {
     bash = {
