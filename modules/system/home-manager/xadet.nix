ea40a04586a1dd6d9556fd07add48c442c1ad71d
FaustXVI <1016863+FaustXVI@users.noreply.github.com>
2023-04-13 23:37:54 +0200

    Start noisetorch at login

--- modules/system/home-manager/xadet.nix
+++ modules/system/home-manager/xadet.nix
@@ -1,7 +1,23 @@
-{ mylib, config, ... }@args:
+{ mylib, config, pkgs, ... }@args:
 {
   imports = mylib.importAllFilteredWith (n: n != "root.nix" && n != "xadet.nix") args ./.;
   config = {
+    systemd.user.services.noisetorch = {
+      Unit = {
+        Description = "Noise torch";
+        After = [ "graphical-session-pre.target" ];
+        PartOf = [ "graphical-session.target" ];
+      };
+
+      Install = { WantedBy = [ "graphical-session.target" ]; };
+      Service = {
+        ExecStart = ["/run/current-system/sw/bin/sleep 0.5"  "${pkgs.noisetorch}/bin/noisetorch -i"];
+        ExecStop = "${pkgs.noisetorch}/bin/noisetorch -u";
+        Type = "oneshot";
+        RemainAfterExit = true;
+      };
+
+    };
     home = {
       stateVersion = config.system.stateVersion;
       enableNixpkgsReleaseCheck = true;
