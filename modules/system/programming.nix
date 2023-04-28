b798c0d6aec2f3b0d07bdc61d7f7d106fa6ec0c4
Xavier Detant <1016863+FaustXVI@users.noreply.github.com>
2023-04-28 16:54:14 +0200

    Add gitui

--- modules/system/programming.nix
+++ modules/system/programming.nix
@@ -4,6 +4,7 @@
   environment = {
     systemPackages = with pkgs; [
       git
+      gitui
       unstable.jetbrains.idea-ultimate
       unstable.jetbrains.pycharm-professional
       unstable.jetbrains.clion
