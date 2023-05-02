1731d4f325e04b4daa7b285e31d2918a77e41a00
FaustXVI <1016863+FaustXVI@users.noreply.github.com>
2023-05-02 18:12:20 +0200

    Add old asus config

---
+++ machines/asus.nix
@@ -0,0 +1,24 @@
+{ config, pkgs, ... }:
+
+let
+  hard = builtins.fetchGit {
+    url = "https://github.com/NixOS/nixos-hardware.git";
+    rev = "b7ac0a56029e4f9e6743b9993037a5aaafd57103";
+  };
+in
+{
+  imports =
+    [
+      "${hard}/common/pc/laptop"
+    ../hardware-configuration.nix
+    ../modules
+  ];
+
+  xadetComputer = {
+    type = "desktop";
+    purposes = [ ];
+    yubikeyAutolock = false;
+  };
+
+  system.stateVersion = "22.11";
+}
