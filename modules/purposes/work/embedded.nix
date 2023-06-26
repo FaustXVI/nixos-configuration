c45984c0efa72eeb53bd617b1023ef835e79886c
Xavier Detant <1016863+FaustXVI@users.noreply.github.com>
2023-06-26 11:20:52 +0200

    Add rights for stm32 card

--- modules/purposes/work/embedded.nix
+++ modules/purposes/work/embedded.nix
@@ -6,6 +6,7 @@
     udev = {
       extraRules = ''
         SUBSYSTEM=="usb", ATTR{idVendor}=="18d1", MODE="0666", GROUP="plugdev"
+        SUBSYSTEM=="usb", ATTR{idVendor}=="0483", MODE="0666", GROUP="plugdev"
       '';
     };
   };
