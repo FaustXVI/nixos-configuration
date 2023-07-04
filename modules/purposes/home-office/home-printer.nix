3a1292bf96c10b194e51bfdfde69521f8fdd4e25
FaustXVI <1016863+FaustXVI@users.noreply.github.com>
2023-07-04 22:27:16 +0200

    Auto detect printer

--- modules/purposes/home-office/home-printer.nix
+++ modules/purposes/home-office/home-printer.nix
@@ -11,23 +11,25 @@
       avahi = {
         enable = true;
         nssmdns = true;
+        openFirewall = true;
       };
       printing = {
         enable = true;
-        drivers = [ pkgs.hplip ];
+        #drivers = with pkgs; [ hplip gutenprint cups-filters ];
+        #browsing = true;
       };
     };
     hardware = {
-      printers = {
-        ensureDefaultPrinter = "home-printer";
-        ensurePrinters = [{
-          name = "home-printer";
-          location = "home";
-          description = "HP DeskJet 3762";
-          deviceUri = "ipp://192.168.1.84";
-          model = "everywhere";
-        }];
-      };
+      #printers = {
+      #  ensureDefaultPrinter = "home-printer";
+      #  ensurePrinters = [{
+      #    name = "home-printer";
+      #    location = "home";
+      #    description = "HP DeskJet 3762";
+      #    deviceUri = "ipp://192.168.1.84/ipp";
+      #    model = "everywhere";
+      #  }];
+      #};
       sane = {
         enable = true;
         extraBackends = [ pkgs.hplipWithPlugin ];
