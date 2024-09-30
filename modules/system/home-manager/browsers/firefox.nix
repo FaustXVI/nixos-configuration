8ae65a6e241ac4d34b03ee639f61080b69c31b86
Xavier Detant <1016863+FaustXVI@users.noreply.github.com>
2024-09-30 09:39:14 +0200

    Add nixos option shortcut

--- modules/system/home-manager/browsers/firefox.nix
+++ modules/system/home-manager/browsers/firefox.nix
@@ -68,6 +68,19 @@ in
                 definedAliases = [ "@np" ];
               };
 
+              "NixOS options" = {
+                urls = [{
+                  template = "https://search.nixos.org/options";
+                  params = [
+                    { name = "type"; value = "packages"; }
+                    { name = "query"; value = "{searchTerms}"; }
+                  ];
+                }];
+
+                icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+                definedAliases = [ "@no" ];
+              };
+
               "NixOS Wiki" = {
                 urls = [{ template = "https://nixos.wiki/index.php?search={searchTerms}"; }];
                 iconUpdateURL = "https://nixos.wiki/favicon.png";
