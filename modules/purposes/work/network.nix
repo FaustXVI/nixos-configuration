c320535afb086d3505e6c42ac2a15e1145787634
Xavier Detant <1016863+FaustXVI@users.noreply.github.com>
2024-12-29 11:37:19 +0100

    Cleaning up

--- modules/purposes/work/network.nix
+++ modules/purposes/work/network.nix
@@ -11,15 +11,5 @@
         enable = true;
       };
     };
-    networking = {
-      firewall = {
-        allowedTCPPorts = [ 8000 8042 ];
-        allowedUDPPorts = [ 8000 8042 ];
-      };
-      extraHosts = ''
-        127.0.0.1 connect.local
-        127.0.0.1 vestalis.local
-      '';
-    };
   };
 }
