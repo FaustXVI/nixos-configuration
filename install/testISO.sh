3c473b1b80caa0033c5d2dc8ad6f81a52ea27a7c
Xavier Detant <1016863+FaustXVI@users.noreply.github.com>
2025-01-02 12:00:10 +0100

    Create scripts for insallation testing

--- install/testISO.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-ISO="$1"
-DISK_NAME="disk-75G.qcow2"
-
-qemu-img create -f qcow2 "$DISK_NAME" 75G
-qemu-system-x86_64 -enable-kvm -m 6G -cdrom "$ISO" -hda "$DISK_NAME" -usb -device u2f-passthru -usb -device usb-ccid 
