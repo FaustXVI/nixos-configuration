fdc9055610fe4aaad29178af972672f9e38833a0
FaustXVI <1016863+FaustXVI@users.noreply.github.com>
2025-01-26 23:54:39 +0100

    Make pointers work everywhere

--- modules/system/home-manager/xadet.nix
+++ modules/system/home-manager/xadet.nix
@@ -9,11 +9,16 @@
         enable = true;
         accent = "dark";
       };
-      accent = "teal";
-      flavor = "mocha";
+      accent = config.catppuccin.accent;
+      flavor = config.catppuccin.flavor;
       enable = true;
     };
+    gtk.enable = true;
     home = {
+      pointerCursor = {
+        x11.enable = true;
+        gtk.enable = true;
+      };
       stateVersion = config.system.stateVersion;
       enableNixpkgsReleaseCheck = true;
     };
