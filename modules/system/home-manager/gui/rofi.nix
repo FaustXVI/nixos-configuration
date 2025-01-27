8bfb93ed30210e9e2df25ccc1ae3e3fd3275e984
Xavier Detant <1016863+FaustXVI@users.noreply.github.com>
2025-01-27 23:04:12 +0100

    First rofi custom

--- modules/system/home-manager/gui/rofi.nix
+++ modules/system/home-manager/gui/rofi.nix
@@ -1,4 +1,24 @@
 { pkgs, lib, config, ... }:
 {
-  programs.rofi.enable = true;
+  programs.rofi = {
+    enable = true;
+    font = lib.lists.head config.fonts.fontconfig.defaultFonts.monospace;
+    terminal = "${lib.getExe pkgs.kitty}";
+    extraConfig = {
+      modi = "run,drun";
+      icon-theme = "Oranchelo";
+      show-icons = true;
+      drun-display-format = "{icon} {name}";
+      location = 0;
+      disable-history = false;
+      hide-scrollbar = true;
+      display-drun = "   Apps ";
+      display-run = "   Run ";
+      display-window = " 󰕰  Window";
+      display-Network = " 󰤨  Network";
+      sidebar-mode = true;
+
+    };
+  };
+
 }
