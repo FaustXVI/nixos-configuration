3a06a42f026846fe52744f9959cd3ef909bdd61a
Xavier Detant <1016863+FaustXVI@users.noreply.github.com>
2025-01-28 18:26:42 +0100

    Formatting

--- modules/system/home-manager/gui/xdph.nix
+++ modules/system/home-manager/gui/xdph.nix
@@ -1,8 +1,8 @@
 { pkgs, lib, config, ... }:
 {
   home.file.".config/hypr/xdph.conf".text = ''
-screencopy {
-    allow_token_by_default = true
-}
-    '';
+    screencopy {
+        allow_token_by_default = true
     }
+  '';
+}
