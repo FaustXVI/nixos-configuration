95353ca835745f6844b9fdd539fe440c02a6af3f
FaustXVI <1016863+FaustXVI@users.noreply.github.com>
2025-02-02 00:26:52 +0100

    print notifications on used screen

--- modules/system/home-manager/gui/dunst.nix
+++ modules/system/home-manager/gui/dunst.nix
@@ -2,6 +2,11 @@
 {
   services.dunst = {
     enable = true;
+    settings = {
+      global = {
+        follow="keyboard";
+      };
+    };
   };
   xdg.configFile = {
     "dunst/dunstrc.d/99-xadet.conf".text = ''
