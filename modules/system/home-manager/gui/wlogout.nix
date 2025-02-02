dd907771cc689d10a366cdfcff4a218d9887cbe6
FaustXVI <1016863+FaustXVI@users.noreply.github.com>
2025-02-02 16:18:34 +0100

    Styling wlogout

--- modules/system/home-manager/gui/wlogout.nix
+++ modules/system/home-manager/gui/wlogout.nix
@@ -4,6 +4,26 @@
     enable = true;
   };
   catppuccin.wlogout.extraStyle = ''
+@import "${pkgs.inputs.catppuccin.packages.${pkgs.system}.waybar}/${config.catppuccin.flavor}.css";
+button {
+    border-radius: 20rem;
+    border: 0;
+    margin: 1rem;
+}
+
+button:hover {
+    box-shadow: 0 0 0.5rem 0 @sapphire;
+}
+
+button:focus {
+    background-color: @mantle;
+}
+button:focus:hover {
+    background-color: @surface0;
+}
+window {
+    background: transparent;
+}
     button {
     border-radius: 10px;
     }
