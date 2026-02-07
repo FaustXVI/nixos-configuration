c96fe2528eab59c895e0e744d3ce57b26715bc77
Xavier Detant <1016863+FaustXVI@users.noreply.github.com>
2026-02-07 11:35:45 +0100

    Fix bambu studio on eove's computer

--- machines/eove.nix
+++ machines/eove.nix
@@ -21,4 +21,34 @@ in
   };
 
   system.stateVersion = "24.11";
+
+  nixpkgs.overlays = [(final: prev: {
+        bambu-studio = prev.appimageTools.wrapType2 rec {
+          name = "BambuStudio";
+          pname = "bambu-studio";
+          version = "02.04.00.70";
+          ubuntu_version = "24.04_PR-8834";
+
+          src = prev.fetchurl {
+            url = "https://github.com/bambulab/BambuStudio/releases/download/v${version}/Bambu_Studio_ubuntu-${ubuntu_version}.AppImage";
+            sha256 = "sha256:26bc07dccb04df2e462b1e03a3766509201c46e27312a15844f6f5d7fdf1debd";
+          };
+
+          profile = ''
+            export SSL_CERT_FILE="${prev.cacert}/etc/ssl/certs/ca-bundle.crt"
+            export GIO_MODULE_DIR="${prev.glib-networking}/lib/gio/modules/"
+          '';
+
+          extraPkgs =
+            pkgs: with pkgs; [
+              cacert
+              glib
+              glib-networking
+              gst_all_1.gst-plugins-bad
+              gst_all_1.gst-plugins-base
+              gst_all_1.gst-plugins-good
+              webkitgtk_4_1
+            ];
+        };
+  })];
 }
