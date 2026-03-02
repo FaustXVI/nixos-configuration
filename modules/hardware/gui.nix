ad762e60645287378c3e4a9db8c8c08ba3597c45
Xavier Detant <1016863+FaustXVI@users.noreply.github.com>
2026-03-02 17:45:00 +0100

    Clean up

--- modules/hardware/gui.nix
+++ modules/hardware/gui.nix
@@ -103,9 +103,5 @@
  
       xdgOpenUsePortal = true;
     };
-    # Fix xdg-portals opening URLs: https://github.com/NixOS/nixpkgs/issues/189851
-    systemd.user.extraConfig = ''
-      DefaultEnvironment="PATH=/run/wrappers/bin:/etc/profiles/per-user/%u/bin:/nix/var/nix/profiles/default/bin:/run/current-system/sw/bin"
-    '';
   };
 }
