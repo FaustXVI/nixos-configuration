55ca3f3627fb97188c342096d5949d67a7a174e6
Xavier Detant <1016863+FaustXVI@users.noreply.github.com>
2026-06-02 16:05:40 +0200

    no quote on background

--- modules/system/home-manager/gui/hyprpaper.nix
+++ modules/system/home-manager/gui/hyprpaper.nix
@@ -1,6 +1,7 @@
 { pkgs, lib, config, ... }:
     let
     conf = pkgs.writeText "hyprpaper.conf" ''
+       splash = false
        wallpaper {
          monitor =
          path = ${./background-image}
