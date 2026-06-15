bd557e89d1c35560bc08287ff004526c5fc48e70
FaustXVI <1016863+FaustXVI@users.noreply.github.com>
2026-06-15 09:38:20 +0200

    Add qwen3.6 as local llm

--- modules/purposes/llm/llm.nix
+++ modules/purposes/llm/llm.nix
@@ -3,22 +3,22 @@
   config = mylib.mkIfComputerHasPurpose "llm" {
   environment.systemPackages = with pkgs; [ opencode ];
     services = {
-      ollama = {
-        enable = false;
-        environmentVariables={
-            OLLAMA_CONTEXT_LENGTH="128000";
-        };
-      };
       llama-cpp = {
-        enable = false;
+        enable = true;
         package = pkgs.llama-cpp-rocm;
+        model = pkgs.requireFile {
+            name =  "Qwen_Qwen3.6-35B-A3B-Q4_K_M.gguf";
+            url = "https://huggingface.co/Abiray/Qwen3.6-35B-A3B-Q4_K_M-GGUF/resolve/main/Qwen3.6-35B-A3B-Q4_K_M.gguf?download=true";
+            hash = "sha256-tG/t0z4L+wyuMIqjwVjQpLLEodIYWh7W8JPNrzkGR3I=";
+        };
+        extraFlags = [
+        "-c" "65536" "-fa" "on" "-ngl" "999" "--n-cpu-moe" "20" "-t" "12" "-b" "2048" "-ub" "2048" "--no-mmap" "--jinja"
+        ];
+        openFirewall = true;
+        port = 11434;
       };
       open-webui = {
         enable = true;
-        port = 1337;
-      };
-      nextjs-ollama-llm-ui = {
-        enable = false;
         port = 1337;
       };
     };
