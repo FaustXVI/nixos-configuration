c555d17e7fb1555ae8947dab1759cd5e12651891
FaustXVI <1016863+FaustXVI@users.noreply.github.com>
2026-08-09 18:16:57 +0200

    Use upstream bambulab

--- machines/desktop-home.nix
+++ machines/desktop-home.nix
@@ -20,51 +20,11 @@ in
           export GALLIUM_DRIVER="zink"
           export WEBKIT_DISABLE_DMABUF_RENDERER=1
         '';
-        version = "2.1.1";
-        appimageName = "Bambu_Studio_ubuntu-24.04_PR-7292.AppImage";
-        zipUrl = "https://github.com/bambulab/BambuStudio/releases/download/v02.01.01.52/BambuStudio_ubuntu-24.04_PR-7292.zip";
-        zipSha256 = "sha256-0xQqDlW0yHnshy6O7nsmOGT49XsLjf6Y8TPnyohp7Sc=";
-        srcZipped = pkgs.fetchzip {
-          url = zipUrl;
-          sha256 = zipSha256;
-        };
-        appimagePath = "${srcZipped}/${appimageName}";
       in
       {
         freecad-wayland = final.writeScriptBin "FreeCAD" ''
           ${exports}
           ${prev.freecad-wayland}/bin/FreeCAD "$@"'';
-        bambu-studio = final.writeScriptBin "bambu-studio" ''
-                              ${exports}
-                              ${prev.lib.getExe (pkgs.appimageTools.wrapType2 {
-            name = "BambuStudio";
-            pname = "bambustudio";
-            inherit version;
-            appimageContents = pkgs.appimageTools.extract {
-              src = appimagePath;
-            };
-            src = appimagePath;
-            profile = ''
-              export SSL_CERT_FILE="${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
-              export GIO_MODULE_DIR="${pkgs.glib-networking}/lib/gio/modules/"
-            '';
-            extraPkgs = pkgs: with pkgs; [
-              cacert
-              curl
-              glib
-              glib-networking
-              gst_all_1.gst-plugins-bad
-              gst_all_1.gst-plugins-base
-              gst_all_1.gst-plugins-good
-              webkitgtk_4_1
-              libglvnd
-              fontconfig
-              dejavu_fonts
-              liberation_ttf
-              libxkbcommon
-              hack-font
-            ];
-          })} "$@"'';
       })
   ];
   xadetComputer = {
