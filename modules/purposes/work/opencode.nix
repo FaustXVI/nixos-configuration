d6181190f4ee495ae48456d05b2769215b51b588
Xavier Detant <1016863+FaustXVI@users.noreply.github.com>
2026-09-08 09:37:47 +0200

    Configure opencode for work

---
+++ modules/purposes/work/opencode.nix
@@ -0,0 +1,23 @@
+{ mylib, config, ... }:
+
+{
+  programs.opencode = {
+    enable = true;
+    settings = {
+      model = "scaleway/qwen3.6-35b-a3b";
+      provider = {
+        scaleway = {
+          apiKey = ''''${FILE:${config.sops.secrets."scaleway.token".path}}'';
+          name = "Scaleway";
+          apiUrl = "https://llm-inference.scaleway.ai/v1";
+          models = {
+            "qwen3.6-35b-a3b" = {
+              name = "qwen3.6-35b-a3b";
+              tool_call = true;
+            };
+          };
+        };
+      };
+    };
+  };
+}
