44e0ed0efd8f8af57e7521c19c994bde848709d7
Xavier Detant <1016863+FaustXVI@users.noreply.github.com>
2026-09-08 15:11:45 +0200

    Fix scaleway api url

--- modules/purposes/work/opencode.nix
+++ modules/purposes/work/opencode.nix
@@ -9,7 +9,7 @@
           scaleway = {
             apiKey = ''''${FILE:${config.sops.secrets."scaleway.token".path}}'';
             name = "Scaleway";
-            apiUrl = "https://llm-inference.scaleway.ai/v1";
+            apiUrl = "https://api.scaleway.ai/v1";
             models = {
               "qwen3.6-35b-a3b" = {
                 name = "qwen3.6-35b-a3b";
